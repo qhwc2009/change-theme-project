eb2c9d12b7f83516fc92b3b4d2e61ac6fc060736
wangchen <wang.chen@mioying.com>
2019-07-11 10:38:11 +0800

    Finish js change theme
    fix some file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { DARK, LIGHT, setTheme } from "prism-color";
+import "./App.styl";
 
+setTheme(DARK);
+
+let i = 0;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="app__demo-box">
+        2123
+        <span
+          className="app__button"
+          onClick={() => {
+            setTheme(i % 2 ? DARK : LIGHT);
+            i++;
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          点击
+        </span>
+      </div>
     </div>
   );
 }
